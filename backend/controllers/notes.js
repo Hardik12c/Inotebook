d56bc274a9e518973bdd981d7fb23cd00a87c6a4
Hardik Garg <93364270+Hardik12c@users.noreply.github.com>
2023-01-07 13:05:40 +0530

    update and delete note added

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,15 +1,16 @@
 const Notesschema = require("../models/Notesschema");
 const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
 
 const getallnotes = async (req, res) => {
-    const Notes=await Notesschema.find({createdby:req.user.userId});
-    res.status(StatusCodes.OK).json({Notes,count:Notes.length});
+  const Notes = await Notesschema.find({ createdby: req.user.userId });
+  res.status(StatusCodes.OK).json({ Notes, count: Notes.length });
 };
 
 const createnote = async (req, res) => {
-    req.body.createdby=req.user.userId;
-    const note=await Notesschema.create(req.body);
-    res.status(StatusCodes.CREATED).json({note});
+  req.body.createdby = req.user.userId;
+  const note = await Notesschema.create(req.body);
+  res.status(StatusCodes.CREATED).json({ note });
 };
 
 const getnote = async (req, res) => {
@@ -17,11 +18,35 @@ const getnote = async (req, res) => {
 };
 
 const updatenote = async (req, res) => {
-  res.status(StatusCodes.OK).json("note updated");
+  const { title, description } = req.body;
+
+  if (title === " " || description === " ") {
+    throw new BadRequestError("Title and description can not be empty");
+  }
+  const note = await Notesschema.findOneAndUpdate(
+    { createdby: req.user.userId, _id: req.params.id },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  if (!note) {
+    throw new NotFoundError(`No Note with ${req.params.id} id`);
+  }
+  res.status(StatusCodes.OK).json(note);
 };
 
 const deletenote = async (req, res) => {
-  res.status(StatusCodes.OK).json("note deleted");
+  const userId = req.user.userId;
+  const noteId = req.params.id;
+
+  const note = await Notesschema.findOneAndDelete({
+    _id: noteId,
+    createdby: userId,
+  });
+
+  if (!note) {
+    throw new NotFoundError(`No Note with ${noteId} id`);
+  }
+  res.status(StatusCodes.OK).json({ note });
 };
 
 module.exports = { getallnotes, getnote, createnote, deletenote, updatenote };
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,8 @@
 const express=require('express');
-const { getallnotes, createnote } = require('../controllers/notes');
+const { getallnotes, createnote, updatenote, deletenote } = require('../controllers/notes');
 
 const router=express.Router();
 
 router.route('/').get(getallnotes).post(createnote); 
-
+router.route('/:id').patch(updatenote).delete(deletenote);
 module.exports=router;
