0c15122ecee982756b8c752cbbd51b942daa13fc
Hardik Garg <93364270+Hardik12c@users.noreply.github.com>
2023-01-11 02:51:59 +0530

    all client side requests centralized

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,20 +1,81 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Notes from "./Notes";
 
 export default function Home() {
-  const [notetext, setnotetext] = useState({
+  // states defined
+  const [notes, setnotes] = useState([]);
+  const [noteinput, setnoteinput] = useState({
     title: "",
     description: "",
     tag: "General",
   });
 
+  // client to server communication
+
+  // get all the notes
+  const getallnotes = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:5000/api/v1/notes/", {
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2M2I4NmRkYTJhMjljMTFmYmM1ZjkxOTMiLCJpYXQiOjE2NzMwMzExMzF9.Z07U1ZAP6fIUUHahQmudYU11IwuSxa9o-kT-KXy7nAs `,
+        },
+      });
+      setnotes(data.Notes);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // createnote
+  const createnote = async (data) => {
+    try {
+      const addednote = await axios.post(
+        "http://localhost:5000/api/v1/notes/",
+        { title: data.title, description: data.description, tag: data.tag },
+        {
+          headers: {
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2M2I4NmRkYTJhMjljMTFmYmM1ZjkxOTMiLCJpYXQiOjE2NzMwMzExMzF9.Z07U1ZAP6fIUUHahQmudYU11IwuSxa9o-kT-KXy7nAs `,
+          },
+        }
+      );
+      console.log(addednote);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // deleting the note
+  const deletenote = async (id) => {
+    try {
+      await axios.delete(`http://localhost:5000/api/v1/notes/${id}`, {
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2M2I4NmRkYTJhMjljMTFmYmM1ZjkxOTMiLCJpYXQiOjE2NzMwMzExMzF9.Z07U1ZAP6fIUUHahQmudYU11IwuSxa9o-kT-KXy7nAs `,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // to fetch all notes in starting
+  useEffect(() => {
+    getallnotes();
+  }, []);
+
+  // handling functions
   const handleinput = (e) => {
-    setnotetext({ ...notetext, [e.target.name]: e.target.value });
+    setnoteinput({ ...noteinput, [e.target.name]: e.target.value });
   };
 
   const handleclick = (e) => {
     e.preventDefault();
-    console.log(notetext);
+    createnote(noteinput);
+    setnoteinput({
+      title: "",
+      description: "",
+      tag: "General",
+    });
   };
   return (
     <div className="container ">
@@ -31,7 +92,7 @@ export default function Home() {
             id="exampleInputEmail1"
             name="title"
             aria-describedby="emailHelp"
-            value={notetext.title}
+            value={noteinput.title}
           />
         </div>
         <div className="mb-3">
@@ -44,13 +105,13 @@ export default function Home() {
             name="description"
             className="form-control"
             id="exampleInputPassword1"
-            value={notetext.description}
+            value={noteinput.description}
           />
         </div>
         <div className="mb-3">
           <label className="form-label">Tag</label>
           <select
-            value={notetext.tag}
+            value={noteinput.tag}
             className="form-select"
             onChange={handleinput}
             name="tag"
@@ -66,7 +127,7 @@ export default function Home() {
         </button>
       </form>
       <h2>Your Notes</h2>
-      <Notes />
+      <Notes notes={notes} getnotes={getallnotes} deletenote={deletenote}/>
     </div>
   );
 }
--- frontend/src/components/Notes.js
+++ frontend/src/components/Notes.js
@@ -1,28 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Noteitem from "./Noteitem";
-import axios from "axios";
-export default function Notes() {
-  const [notes, setnotes] = useState([]);
-  const getallnotes = async () => {
-    try {
-      const { data } = await axios.get("http://localhost:5000/api/v1/notes/", {
-        headers: {
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2M2I4NmRkYTJhMjljMTFmYmM1ZjkxOTMiLCJpYXQiOjE2NzMwMzExMzF9.Z07U1ZAP6fIUUHahQmudYU11IwuSxa9o-kT-KXy7nAs `,
-        },
-      });
-      setnotes(data.Notes);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    getallnotes();
-  }, []);
-
+export default function Notes(props) {
   return (
     <div className="row">
-      {notes.map((note) => (
-        <Noteitem key={note._id} note={note} />
+      {props.notes.map((note) => (
+        <Noteitem key={note._id} note={note} deletenote={props.deletenote} />
       ))}
     </div>
   );
