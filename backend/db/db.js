acaad56050fc18624b2e793bedadc7b02f8e2956
Hardik <hardik12c@gmail.com>
2023-01-12 22:46:55 +0530

    db mongoose updated to version 6

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
+mongoose.set("strictQuery", false);
 
 const connectDB = (url) => {
   return mongoose.connect(url, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
     useUnifiedTopology: true,
   })
 }
